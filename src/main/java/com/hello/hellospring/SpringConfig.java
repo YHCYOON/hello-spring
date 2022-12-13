9aa17b25efec50792ebd167f8b4941cabe8a040a
윤희찬 <yhc920923@gmail.com>
2022-12-13 20:50:35 +0900

    JPA

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.repository.JpaMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
